2d8777dfe6dbefdaa7d6fa8ec26bdb8b7a908421
Omer Sherer <omer.sherer141@gmail.com>
2022-09-02 18:37:10 +0300

    c

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     main();
-    $("#container").html("<h1>javascript file is workning!</h1>");
+    $("#container").html("<h1>javascript file is workning</h1>");
 });
 
 function main(){
     //document.getElementById("container").innerHTML = "<h1>javascript file is workning!</h1>";
 };
 
+
+// Button links
 function go_to_simulation(){
 
 }
